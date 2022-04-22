33f257088e28bfaa2f6e4ee20345cca2dd6a0a43
Preeti0507 <83224527+Preeti0507@users.noreply.github.com>
2022-04-22 12:57:03 +0530

    Initial commit

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,12 @@
+package org.example;
+import java.util.*;
+import java.sql.SQLOutput;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        Scanner sc=new Scanner(System.in);
+        int a=sc.nextInt();
+        System.out.println(a);
+    }
+}
